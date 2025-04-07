247dc774a36f79572f2a7762a351d97ac0d35c7e
jzitounifla <jzitounifla@ensc.fr>
2025-04-07 22:56:02 +0200

    testsPlantes

--- ENSemenCe/Plante.cs
+++ ENSemenCe/Plante.cs
@@ -1,9 +1,26 @@
+using System.ComponentModel;
+
 public abstract class Plante
 {
     public new string[] Design { get; set; }
     public new string[] Etat { get; set; }
+    public new int[] NecessaireConstruction { get; set; }
+    public new int[] ResultatDemontage { get; set; }
+    public new int NombreFruits { get; set; }
     public Plante()
     {
 
     }
 }
+
+
+/*
+  
+~○⦿○~
+○⦿○○⦿
+  |
+  |
+ ———
+
+
+*/
