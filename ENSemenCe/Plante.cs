2782829f5d49210056b79a098b513a4ba2d6ef7e
jzitounifla <jzitounifla@ensc.fr>
2025-05-21 11:14:01 +0200

    calcul de l'état de la plante

--- ENSemenCe/Plante.cs
+++ ENSemenCe/Plante.cs
@@ -69,7 +69,7 @@ public abstract class Plante
     TypePlante = "PoMWier"; //marguerite par défaut, mais pas de raison que soit appelé. 
     Parcelle = parcelle;
     Coord = [x, y];
-    EspacementNecessaire = 0;
+    EspacementNecessaire = Convert.ToInt16(DonneesPlantes.PlantesRessources[TypePlante]["Espace"]);
     DureeVie = [0, Constantes.PlantesDureeVie[TypePlante]]; //en 0 le temps de vie vécu (ici 0 mois), en 1 le total (plus facile à accéder par la suite)
   }
 
@@ -93,8 +93,6 @@ public abstract class Plante
     NiveauElectricite += DonneesClimatiques.TousLesMois[mois].ElectrisationMoyenne;
     NiveauUV += DonneesClimatiques.TousLesMois[mois].Ensoleillement;
 
-
-
     //UTILISE DES TRUCS 
     NiveauHuile -= DonneesPlantes.PlantesRessources[TypePlante]["huile"] / 100 * (DonneesClimatiques.TousLesMois[mois].TemperatureMoyenne * 0.85); //utilise de l'huile
     NiveauElectricite -= DonneesPlantes.PlantesRessources[TypePlante]["electricite"] / 3; //le niveau d'électricité se fait diviser par 3
@@ -103,10 +101,61 @@ public abstract class Plante
 
 
   public int CalculerEtat(int mois, Parcelle parcelle) //Calculer l'état de la plante selon la météo du mois et la parcelle où elle est plantée
+  /*
+  Renvoie un entier : 
+  3 => en très bonne santé
+  2 => en bonne santé
+  1 => en état dangereux 
+  0 => Morte. 
+  */
   {
-    //Etat en fonction de l'élécrisation
-    int a = 1;
-    return a;
+    //Etat en fonction de la duree de vie : fragilité de la plante augmente plus elle est jeune ou plus elle est vieille. 
+    double fragilite = 0; //de base, vaut 0. 
+    if (DureeVie[1] / 3 > DureeVie[0]) //si est dans le premier tiers de sa vie
+    {
+      fragilite = DureeVie[0] / (DureeVie[1] / 3);
+    }
+    if (DureeVie[1] - (DureeVie[1] / 3) < DureeVie[0]) //si est dans le dernier tiers de sa vie
+    {
+      fragilite = 1 - (DureeVie[0] / (DureeVie[1] / 3));
+    }
+
+    //ETAT EN FONCTION DE L'HUILE : doit être à ±10% pour très bien, ±20% pour bien, sinon mauvais
+    int etatHuile = 1;
+    //si on est à ±20% du niveau d'huile requis
+    if (NiveauHuile > DonneesPlantes.PlantesRessources[TypePlante]["huile"] - (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauHuile < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+    {
+      //si on est dans les 10% 
+      if (NiveauHuile > DonneesPlantes.PlantesRessources[TypePlante]["huile"] - (0.1 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauHuile < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.1 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+        etatHuile = 3;
+      else etatHuile = 2;
+    }
+
+    //Etat en fonction de l'ELECTRISATION
+    int etatElectrisation = 1;
+    //si on est à ±20% du niveau d'électricite requis
+    if (NiveauElectricite > DonneesPlantes.PlantesRessources[TypePlante]["electricite"] - (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauElectricite < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+    {
+      //si on est dans les 10% 
+      if (NiveauElectricite > DonneesPlantes.PlantesRessources[TypePlante]["electricite"] - (0.1 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauElectricite < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.1 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+        etatElectrisation = 3;
+      else etatElectrisation = 2;
+    }
+
+    //Etat en fonction du niveau UV
+    int etatUV = 1;
+    //si on est à ±50% du niveau d'UV requis
+    if (NiveauUV > DonneesPlantes.PlantesRessources[TypePlante]["electricite"] - (0.5 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauUV < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.5 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+    {
+      //si on est dans les 20% 
+      if (NiveauUV > DonneesPlantes.PlantesRessources[TypePlante]["electricite"] - (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]) && NiveauUV < DonneesPlantes.PlantesRessources[TypePlante]["huile"] + (0.2 * DonneesPlantes.PlantesRessources[TypePlante]["huile"]))
+        etatUV = 3;
+      else etatUV = 2;
+    }
+
+    int etatGeneral = Convert.ToInt16((3 + ((0.5 * fragilite + 0.5) * ((etatHuile + etatElectrisation + etatUV) / 3))) / (1 + (0.5 * fragilite + 0.5)));
+
+    return etatGeneral;
   }
 
 
